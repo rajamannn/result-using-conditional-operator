ff293363c6b107f5af82a95e757f9f326c2ec979
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-04-14 17:23:53 +0530

    Add files via upload

---
+++ Conditional Operator.c
@@ -0,0 +1,14 @@
+//program to find result using Conditional Operator
+ //conditional operator=?     :
+
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter the number:");
+    scanf("%d",&a);
+
+(a>=100)? printf("Pass") : printf("Fail");
+
+return 0;
+}
